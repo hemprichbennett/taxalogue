9a47e6139a4ff8f4ee01465ad65e65c2dc17958f
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-01-10 09:20:22 +0000

    Added sif_build script and logfile directory, for cluster use

---
+++ sif_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=taxalogue_container # the name for the cluster scheduler
+#SBATCH --time=01:30:00 # Maximum allowed runtime per iteration
+#SBATCH --output=logfiles/taxalogue_cont_gen.out # the name of the output files
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=50G
+#SBATCH --mail-user=david.hemprich-bennett@biology.ox.ac.uk
+
+
+# Create an environment in $DATA and give it an appropriate name
+export SINGULARITY_CACHEDIR=$DATA/sif_lib/
+
+singularity pull -F docker://hemprichbennett/taxalogue_img:2024-01-08 bash
