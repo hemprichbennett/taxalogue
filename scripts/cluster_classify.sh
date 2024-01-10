3c578b3a4b8ce857191ce4beedcf47e07ba8a8e5
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-01-10 10:52:33 +0000

    Edited filepath

--- scripts/cluster_classify.sh
+++ scripts/cluster_classify.sh
@@ -1,3 +1,3 @@
-cd home/taxalogue
+cd /home/taxalogue-1.0.0
 
 bundle exec ruby taxalogue.rb --taxon Arthropoda classify --gbol --bold_release downloads/BOLD_Public.01-Jan-2024.tsv --genbank_dir downloads/NCBIGENBANK/release259
