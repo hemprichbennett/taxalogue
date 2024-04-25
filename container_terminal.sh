6587ba8f10f051e81c418120fdd4f7fc9567a070
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-04-25 09:19:28 +0100

    Updated bind paths

--- container_terminal.sh
+++ container_terminal.sh
@@ -1,1 +1,5 @@
-docker run -i -t -v  /Volumes/Extreme_SSD/data/taxalogue/taxalogue-1.0.0/downloads:/home/taxalogue-1.0.0/downloads/ -v /Volumes/Extreme_SSD/data/taxalogue/taxalogue-1.0.0/results:/home/taxalogue-1.0.0/results/ hemprichbennett/taxalogue_img:2024-01-08 bash
+docker run -i -t -v  \
+/Volumes/oxford_SSD/data/taxalogue/taxalogue-1.0.0/downloads:/home/taxalogue-1.0.0/downloads/ \
+-v /Volumes/oxford_SSD/data/bold_downloads:/home/bold_downloads/ \
+-v /Volumes/oxford_SSD/data/taxalogue/taxalogue-1.0.0/results:/home/taxalogue-1.0.0/results/ \
+hemprichbennett/taxalogue_img:2024-01-08 bash
