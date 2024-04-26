0d82976943c23f92fb269542376ceaec5c152aec
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-04-26 10:32:35 +0100

    Piped yes command to interactive prompt

--- scripts/cluster_classify.sh
+++ scripts/cluster_classify.sh
@@ -1,3 +1,5 @@
 cd /home/zool2291/projects/taxalogue/taxalogue-1.0.0
 
-bundle exec ruby taxalogue.rb --taxon Arthropoda classify --gbol --bold_release downloads/BOLD_Public.01-Jan-2024.tsv --genbank_dir downloads/NCBIGENBANK/release259
+# piping yes command in for now, as taxalogue asks for verification of wanting
+# to use an old genbank release when a new one is available
+yes | bundle exec ruby taxalogue.rb --taxon Arthropoda classify --gbol --bold_release downloads/BOLD_Public.01-Jan-2024.tsv --genbank_dir downloads/NCBIGENBANK/release259
