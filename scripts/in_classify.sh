b60ab94496315ac1c80b9a702b956e2d45289b11
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-05-17 11:32:50 +0100

    Increased tasks per node

--- scripts/in_classify.sh
+++ scripts/in_classify.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=logfiles/%A_taxalogue_classify.out # the name of the output files
 #SBATCH --mail-type=ALL
 #SBATCH --mem-per-cpu=30G
-#SBATCH --ntasks-per-node=20
+#SBATCH --ntasks-per-node=40
 #SBATCH --mail-user=david.hemprich-bennett@biology.ox.ac.uk
 
 
