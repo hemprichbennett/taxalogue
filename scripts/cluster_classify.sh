5a1f3afd589667624a9d0f33cf0bdda8777dc478
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-05-23 11:24:47 +0100

    Altered minimum taxon rank from family to genus

--- scripts/cluster_classify.sh
+++ scripts/cluster_classify.sh
@@ -2,4 +2,4 @@ cd /home/zool2291/projects/taxalogue/taxalogue-1.0.0
 
 # piping yes command in for now, as taxalogue asks for verification of wanting
 # to use an old genbank release when a new one is available
-yes | bundle exec ruby taxalogue.rb --taxon Arthropoda classify --gbol --bold_release downloads/BOLD_Public.01-Jan-2024.tsv --genbank_dir downloads/NCBIGENBANK/release259 --taxon_rank family
+yes | bundle exec ruby taxalogue.rb --taxon Arthropoda classify --gbol --bold_release downloads/BOLD_Public.01-Jan-2024.tsv --genbank_dir downloads/NCBIGENBANK/release259 --taxon_rank genus
