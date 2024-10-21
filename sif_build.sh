aa4f6911e739542f2e1a564d659f861ab191fc7d
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-10-21 13:52:04 +0100

    Updated image tag

--- sif_build.sh
+++ sif_build.sh
@@ -11,4 +11,4 @@
 # Create an environment in $DATA and give it an appropriate name
 export SINGULARITY_CACHEDIR=$DATA/sif_lib/
 
-singularity pull -F docker://hemprichbennett/taxalogue_img:2024-10-18
+singularity pull -F docker://hemprichbennett/taxalogue_img:2024-10-21
