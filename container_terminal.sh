75964a99c93ee80cbb2707952ec519460172876a
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-10-22 16:25:50 +0100

    Changed projects home path in container from /home to /home/zool2291

--- container_terminal.sh
+++ container_terminal.sh
@@ -1,6 +1,6 @@
 docker run -i -t -v  \
-/Volumes/oxford_SSD/data/taxalogue/taxalogue-1.0.0/downloads:/home/taxalogue-1.0.0/downloads/ \
--v /Volumes/oxford_SSD/data/bold_downloads:/home/bold_downloads/ \
--v /Volumes/oxford_SSD/data/taxalogue/taxalogue-1.0.0/results:/home/taxalogue-1.0.0/results/ \
--v /Users/dhb/Dropbox/work/projects/in_progress/taxalogue/taxalogue-1.0.0/default_config.yaml:/home/taxalogue-1.0.0/default_config.yaml \
-hemprichbennett/taxalogue_img:2024-10-17 bash
+/Volumes/oxford_SSD/data/taxalogue/taxalogue-1.0.0/downloads:/home/zool2291/taxalogue-1.0.0/downloads/ \
+-v /Volumes/oxford_SSD/data/bold_downloads:/home/zool2291/bold_downloads/ \
+-v /Volumes/oxford_SSD/data/taxalogue/taxalogue-1.0.0/results:/home/zool2291/taxalogue-1.0.0/results/ \
+-v /Users/dhb/Dropbox/work/projects/in_progress/taxalogue/taxalogue-1.0.0/default_config.yaml:/home/zool2291/taxalogue-1.0.0/default_config.yaml \
+hemprichbennett/taxalogue_img:2024-10-22 bash
