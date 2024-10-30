053a270d5e0faf4a823866d617ba8ba99dab5cf6
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2024-10-30 10:07:30 +0000

    Reduced number of nodes requested

--- scripts/in_classify.sh
+++ scripts/in_classify.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=logfiles/%A_taxalogue_classify.out # the name of the output files
 #SBATCH --mail-type=ALL
 #SBATCH --mem-per-cpu=160G
-#SBATCH --nodes=120
+#SBATCH --nodes=10
 #SBATCH --mail-user=david.hemprich-bennett@biology.ox.ac.uk
 
 
